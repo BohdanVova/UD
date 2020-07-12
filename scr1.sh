11614581fada78df6a967dfdefe84ca9e6d97e2c
BohdanVova <volodiabohdan867@gmail.com>
2020-07-12 21:31:05 +0300

    Add scr1.sh

---
+++ scr1.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+file="$@"
+
+if [ -f "$file" ]
+then
+	echo "File $file exist"
+	chmod u+x $file
+	ls -l | grep "$file"
+elif [ "$*" \> 1 ]
+then
+	echo "Enter only the same file name!!!"
+elif [ "$*"==0 ]
+then
+	echo "You haven't entered anything!!!"
+	echo "Enter a file name!!!"
+fi
+# UD
