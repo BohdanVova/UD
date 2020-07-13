cf5fe83d89f539cb53c8c961385befcce6c898d3
BohdanVova <volodiabohdan867@gmail.com>
2020-07-13 15:34:42 +0300

    Add scr3.sh

--- scr3.sh
+++ scr3.sh
@@ -4,7 +4,7 @@ do
 if [ -d $files ]
 then
 name=$(basename "$files")
-	echo -n "$name: " >> /home/volodymyr/dr_nm_fl.txt
-        find "$files"  -type f |  wc -l  >> /home/volodymyr/dr_nm_fl.txt
+	echo -n "$name: "
+        find "$files"  -type f |  wc -l
 fi
-done
+done >> /home/volodymyr/dr_num_fl.txt
